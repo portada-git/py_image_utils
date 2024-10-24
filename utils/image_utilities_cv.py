e173881194e85aa57198115c6829e54c91e0adca
Orlando Grabiel Toledano Lopez <ogtoledano@uci.cu>
2024-10-24 10:58:18 -0500

    Add new: convert_ordered_block_stack_to_cv2 function for encoding ndarray collection blocks into cv2 image

--- utils/image_utilities_cv.py
+++ utils/image_utilities_cv.py
@@ -1,6 +1,36 @@
 import cv2
 import numpy as np
 import os
+from typing import Tuple
+
+
+def get_block_coordinates(block: dict, img_width: int,
+                          img_height: int) -> Tuple[float, float,
+                                                    float, float]:
+    """
+    Calculate the coordinates of a block within an image.
+
+    Parameters:
+        block (dict): A dictionary representing the block with 'bounds'
+                      information.
+        img_width (int): The width of the image with the blocks.
+        img_height (int): The height of the image with the blocks.
+
+    Returns:
+        Tuple[float, float, float, float]: A tuple representing the
+                                           coordinates of the block
+                                           (xmin, ymin, width, height).
+    """
+
+    bounds = block['bounds']
+    xmin = int(bounds[0] * img_width)
+    ymin = int(bounds[1] * img_height)
+    xmax = int(bounds[2] * img_width)
+    ymax = int(bounds[3] * img_height)
+    w = xmax - xmin
+    h = ymax - ymin
+    return xmin, ymin, w, h
+
 
 def resize_image_percent(url_image_src, url_image_trg, percent=0.75):
     """
@@ -85,3 +115,44 @@ def rescale_mask(layout_mask_arcanum, original_width, original_height):
     layout_mask = cv2.resize(layout_mask_arcanum, (original_width, original_height), interpolation=cv2.INTER_NEAREST)
 
     return layout_mask
+
+
+def convert_ordered_block_stack_to_cv2(image: np.ndarray, blocks: list[dict]):
+    """
+       Convert an ordered block stack to a list of OpenCV images.
+
+       Args:
+           image (numpy.ndarray): The original image as a numpy array.
+           blocks (list[dict]): The ordered block stack as a list of dictionaries, where each dictionary represents a block and contains keys "x", "y", "w", and "h".
+
+       Returns:
+           list[numpy.ndarray]: A list of OpenCV images, where each image corresponds to a block in the ordered block stack.
+    """
+
+    img_height, img_width, _ = image.shape
+
+    cut_blocks = []
+    max_width = 0
+    cut_blocks_cv2 = []
+
+    for block in blocks:
+        x1, y1, w, h = get_block_coordinates(block, img_width, img_height)
+
+        # Calculate the bottom-right corner coordinates of each block
+        x2 = x1 + w
+        y2 = y1 + h
+
+        # Cut the text block region from the image
+        cut_block = image[y1:y2, x1:x2]
+
+        # Track the maximum width of the blocks
+        if cut_block.shape[1] > max_width:
+            max_width = cut_block.shape[1]
+
+        # Append the cut block to the list and transform into image cv2
+        cut_blocks.append(cut_block)
+        image_block = cv2.cvtColor(cut_block, cv2.COLOR_GRAY2BGR)
+        image_cv2 = cv2.imdecode(image_block.astype(np.uint8), cv2.IMREAD_GRAYSCALE)
+        cut_blocks_cv2.append(image_cv2)
+
+    return cut_blocks_cv2
