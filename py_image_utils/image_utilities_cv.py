41c01b68e8e7dfd15733445ffc68f1bd1e152de0
jcanell4 <jcanell4@xtec.cat>
2024-10-28 22:27:00 +0100

    Coments to recode

--- py_image_utils/image_utilities_cv.py
+++ py_image_utils/image_utilities_cv.py
@@ -1,12 +1,11 @@
 import cv2
 import numpy as np
-import os
 from typing import Tuple
 
 
 def get_block_coordinates(block: dict, img_width: int,
                           img_height: int) -> Tuple[float, float,
-                                                    float, float]:
+float, float]:
     """
     Calculate the coordinates of a block within an image.
 
@@ -32,6 +31,8 @@ def get_block_coordinates(block: dict, img_width: int,
     return xmin, ymin, w, h
 
 
+# JCB NOTE: Following the logic of the other functions, we need to remove the second parameter and return an image in
+# cv2 format without save it to disk.
 def resize_image_percent(url_image_src, url_image_trg, percent=0.75):
     """
     Resizes an image by a given percentual ratio.
@@ -53,9 +54,9 @@ def resize_image_percent(url_image_src, url_image_trg, percent=0.75):
     return img_per, img.shape[1], img.shape[0]
 
 
-#
-# def resize_image_percent_til_size(image_src, url_image_trg, nested_size=6291456): # 6MB  JCB NOTE-IMPORTANT: the second parameter is unnecessary!
-def resize_image_percent_til_size(image_src, nested_size=6291456):
+# JCB NOTE-IMPORTANT: the second parameter (url_image_trg) is unnecessary! The documentation must be changed
+# def resize_image_percent_til_size(image_src, url_image_trg, nested_size=6291456): # 6MB
+def resize_image_percent_til_size(image_src, nested_size=6291456):  # 6MB
     """
     Resizes an image by a percentual ratio until reaches max_size.
 
@@ -75,11 +76,13 @@ def resize_image_percent_til_size(image_src, nested_size=6291456):
     if size_in_bytes > nested_size:
         img_per = cv2.resize(image_src, None, fx=percent, fy=percent)
         # cv2.imwrite(f'{url_image_trg}_resized.jpg', img_per)
-        return img_per, image_src.shape[1], image_src.shape[0], percent # Returns the original 2-D dimensions
+        return img_per, image_src.shape[1], image_src.shape[0], percent  # Returns the original 2-D dimensions
 
-    return image_src, image_src.shape[1], image_src.shape[0], 1 # Image without changes
+    return image_src, image_src.shape[1], image_src.shape[0], 1  # Image without changes
 
 
+# JCB NOTE: Following the logic of the other functions, we need to remove the second parameter and return an image in
+# cv2 format without save it to disk.
 def reduce_image_resolution(url_image_src, url_image_trg, quality=30):
     """
     Reduce the resolution of an image by saving it with a lower quality JPEG compression.
@@ -102,6 +105,7 @@ def reduce_image_resolution(url_image_src, url_image_trg, quality=30):
     return img_low, img.shape[1], img.shape[0]
 
 
+# JCB NOTE: I think that this function is unnecessary and should be removed to avoid misunderstanding
 def rescale_mask(layout_mask_arcanum, original_width, original_height):
     """
         Rescale a layout mask to match the original image size.
@@ -132,12 +136,12 @@ def convert_ordered_block_stack_to_cv2(image: np.ndarray, blocks: list[dict]):
     """
 
     img_height, img_width, _ = image.shape
-# JCB NOTE - IMPORTANT: The image arrives in CV2 format. It is not necessary to convert it. Or if you want,
+    # JCB NOTE - IMPORTANT: The image arrives in CV2 format. It is not necessary to convert it. Or if you want,
     # before converting it, you should check if the format is PIL or CV2.
 
     cut_blocks = []
-#    max_width = 0   JCB NOTE: it's not necessary
-#    cut_blocks_cv2 = []  JCB NOTE-IMPORTANT: if it's not necessary to convert image, you can use the existing array named cut_blocks
+    #    max_width = 0   JCB NOTE: it's not necessary
+    #    cut_blocks_cv2 = []  JCB NOTE-IMPORTANT: if it's not necessary to convert image, you can use the existing array named cut_blocks
 
     for block in blocks:
         x1, y1, w, h = get_block_coordinates(block, img_width, img_height)
@@ -149,17 +153,17 @@ def convert_ordered_block_stack_to_cv2(image: np.ndarray, blocks: list[dict]):
         # Cut the text block region from the image
         cut_block = image[y1:y2, x1:x2]
 
-# JCB NOTE: max_width does not any function here, please delete
+        # JCB NOTE: max_width does not any function here, please delete
         # # Track the maximum width of the blocks
         # if cut_block.shape[1] > max_width:
         #     max_width = cut_block.shape[1]
 
         # Append the cut block to the list and transform into image cv2
         cut_blocks.append(cut_block)
-#    JCB NOTE - IMPORTANT:  if it's not necessary to convert image, next three lines must be removed
-#        image_block = cv2.cvtColor(cut_block, cv2.COLOR_RGB2BGR)
-#        image_cv2 = cv2.imdecode(image_block.astype(np.uint8), cv2.IMREAD_COLOR)
-#        cut_blocks_cv2.append(image_cv2)
+    #    JCB NOTE - IMPORTANT:  if it's not necessary to convert image, next three lines must be removed
+    #        image_block = cv2.cvtColor(cut_block, cv2.COLOR_RGB2BGR)
+    #        image_cv2 = cv2.imdecode(image_block.astype(np.uint8), cv2.IMREAD_COLOR)
+    #        cut_blocks_cv2.append(image_cv2)
 
-#    return cut_blocks_cv2
+    #    return cut_blocks_cv2
     return cut_blocks
